74a32ce619debdacfbaef5d8b3f953c58512232f
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-12 18:47:15 -0300

    adiciona schema de objeto usuario usando yup

--- src/apps/user/user.schema.ts
+++ src/apps/user/user.schema.ts
@@ -1,4 +1,5 @@
-interface IUser {
+import yup from 'yup'
+export interface IUser {
     id?: number,
     username: string,
     password: string,
@@ -6,3 +7,9 @@ interface IUser {
     name?: string
 }
 
+export const userSchema = yup.object().shape({
+    username: yup.string().required().max(128).min(2),
+    password: yup.string().required().max(255).min(2),
+    email: yup.string().email().required().max(128),
+    name: yup.string().max(255)
+})
