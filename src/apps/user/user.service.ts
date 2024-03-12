e7a773a91e54d98de9908eba725b72e5dda652e6
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-03-12 18:48:00 -0300

    adiciona todos os metodos necessarios mockados para usar no service

--- src/apps/user/user.service.ts
+++ src/apps/user/user.service.ts
@@ -1,11 +1,51 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpMethod, requestMethodAccept } from "../../middlewares/requestMethodAccept.middleware";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export function userFindManyService(request: Request, response: Response, next: NextFunction) {
     try {
         requestMethodAccept(HttpMethod.GET)(request, response, next);
         return response.json({"message": "get many users from service", "method":request.method})
     } catch (error) {
-        return response.json({error})
+        return response.json({error}).status(500);
     }
+}
+
+
+export function userFindOneService(request: Request, response: Response, next: NextFunction) {
+    try {
+        requestMethodAccept(HttpMethod.GET)(request, response, next);
+        return response.json({"message": "get one user from service", "method":request.method})
+    } catch (error) {
+        return response.json({error}).status(500);
+    }
+}
+
+export function userCreateService(request: Request, response: Response, next: NextFunction) {
+    try {
+        requestMethodAccept(HttpMethod.POST)(request, response, next);
+        return response.json({"message": "create an user from service", "method":request.method})
+    } catch (error) {
+        return response.json({error}).status(500);
+    }
+}
+
+export function userUpdateService(request: Request, response: Response, next: NextFunction) {
+    try {
+        requestMethodAccept(HttpMethod.PUT)(request, response, next);
+        return response.json({"message": "update an user from service", "method":request.method})
+    } catch (error) {
+        return response.json({error}).status(500);
+    }
+}
+
+export function userDeleteService(request: Request, response: Response, next: NextFunction) {
+    try {
+        requestMethodAccept(HttpMethod.DELETE)(request, response, next);
+        return response.json({"message": "delete an user from service", "method":request.method})
+    } catch (error) {
+        return response.json({error}).status(500);
+    }
+    
 }
